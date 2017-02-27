6db55c75a024bb53e2422fdd633613477dd9c913
Michael_Hall <hallmichaelp@gmail.com>
2017-02-27 14:59:46 -0500

    final edits to files

---
+++ generator.php
@@ -0,0 +1,64 @@
+<!-- PHP Initialization Section -->
+<?php
+# -- Require Password class file
+require('Password.php');
+# --Instantiate a new password object and pass in form input/call constructor method
+$password = new Password($_POST['password_length'], $_POST['case'], !empty($_POST['numbers']), !empty($_POST['special_characters']));
+?>
+
+<!-- Start of HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include ('includes/head.php');?>
+</head>
+<body>
+
+  <!-- Header Section -->
+
+  <div class="container">
+    <header>
+      <div class="row">
+        <div class="twelve columns">
+          <h1>Password Generator</h1>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Output Section -->
+
+    <div class="row">
+      <div class="four columns">
+          <h5>Your random password is: </h5>
+      </div>
+      <div class="eight columns">
+        <h4 class="password"><?=$password->getUserPassword()?></h4>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="twelve columns">
+        <input class="button" type="submit" value="Copy to Clipboard">
+      </div>
+    </div>
+
+    <!-- Supplementary Details -->
+
+    <div class="row pushDown">
+      <div class="twelve columns">
+        <p>Your password includes the following specifications:</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="twelve columns">
+        <ul>
+          <li>Length: <?=$password->getLength()?></li>
+          <li>Case: <?=$password->getCase()?></li>
+          <li>Numbers: <?=$password->getNumbers()?></li>
+          <li>Special Characters: <?=$password->getSpecialChars();?></li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -5,49 +5,43 @@
 </head>
 <body>
 
-  <!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <!-- Header Section -->
+
   <div class="container">
     <header>
       <div class="row">
         <div class="twelve columns">
-          <h1>Password Creator</h1>
+          <h1>Password Generator</h1>
         </div>
       </div>
     </header>
 
     <h5>Here are the general instructions for creating a password.</h5>
 
+    <!-- Form Section -->
+
     <form method='post' action='generator.php'>
         <div class="row">
           <div class="twelve columns">
-            <label for="length">Password Length:</label>
+            <label for="length">* Password Length:</label>
             <input type="number" id="length" name="password_length" min="5" max="20" value="5" required></input>
+            <p class="italic">Length must be between 5 and 20 characters.</p>
           </div>
         </div>
 
         <div class="row">
           <div class="twelve columns">
-            <label for="case">Case:</label>
-            <select id="case" name="character_case" required>
-              <option value="hard">Upper and Lower Case</option>
-              <option value="easy">Lower Case Only</option>
-              <option value="moderate">Upper Case Only</option>
-            </select>
+            <label for="case">* Letter Case:</label>
+            <input type="radio" name="case" value="lower_case" checked> Lower Only</input>
+            <input type="radio" name="case" value="mixed_case"> Upper and Lower</input>
           </div>
         </div>
 
         <div class="row">
           <div class="twelve columns">
-            <label>Numbers:</label>
-            <input type="checkbox" name="numbers"></input>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="twelve columns">
-            <label>Special Characters:</label>
-            <input type="checkbox" name="special_characters"></input>
+            <label>Include:</label>
+            <input type="checkbox" name="numbers"> Numbers</input>
+            <input type="checkbox" name="special_characters"> Special Characters</input>
           </div>
         </div>
 
@@ -57,8 +51,9 @@
           </div>
         </div>
       </form>
+
+      <p class="italic">* Indicates a required field.</p>
+
     </div>
-<!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-</body>
+  </body>
 </html>
