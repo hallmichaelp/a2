64a1096906a37a63b1e4872e247f4df5db7dc1b9
Michael_Hall <hallmichaelp@gmail.com>
2017-02-27 19:27:30 -0500

    updated index page based on validation results

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <div class="row">
           <div class="twelve columns">
             <label for="length">* Password Length:</label>
-            <input type="number" id="length" name="password_length" min="5" max="20" value="5" required></input>
+            <input type="number" id="length" name="password_length" min="5" max="20" value="5" required>
             <p class="italic">Length must be between 5 and 20 characters.</p>
           </div>
         </div>
@@ -32,16 +32,16 @@
         <div class="row">
           <div class="twelve columns">
             <label for="case">* Letter Case:</label>
-            <input type="radio" name="case" value="lower_case" checked> Lower Only</input>
-            <input type="radio" name="case" value="mixed_case"> Upper and Lower</input>
+            <input type="radio" name="case" value="lower_case" checked> Lower Only
+            <input type="radio" name="case" value="mixed_case"> Upper and Lower
           </div>
         </div>
 
         <div class="row">
           <div class="twelve columns">
             <label>Include:</label>
-            <input type="checkbox" name="numbers"> Numbers</input>
-            <input type="checkbox" name="special_characters"> Special Characters</input>
+            <input type="checkbox" name="numbers"> Numbers
+            <input type="checkbox" name="special_characters"> Special Characters
           </div>
         </div>
 
